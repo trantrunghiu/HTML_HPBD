86fb4d80f1fc340386b6951cdadaaf60e86333a0
Trung Hiếu <trantrunghieu.mail@gmail.com>
2023-12-14 03:09:23 +0700

    Change picture

--- js/index.js
+++ js/index.js
@@ -6,20 +6,26 @@
   var card = $('card'),
       openB = $('open'),
       closeB = $('close'),
-      timer = null;
-  console.log('wat', card);
+      timer = null,
+      audio = $('myAudio'); // Thêm đoạn này để truy cập đối tượng âm thanh
+
   openB.addEventListener('click', function () {
+    // Các dòng code mở card
     card.setAttribute('class', 'open-half');
     if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', 'open-fully');
       timer = null;
     }, 1000);
+
+    // Phát âm thanh khi nhấp vào nút "Open"
+    audio.play();
   });
 
   closeB.addEventListener('click', function () {
+    // Các dòng code đóng card
     card.setAttribute('class', 'close-half');
-    if (timer) clearTimerout(timer);
+    if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', '');
       timer = null;
